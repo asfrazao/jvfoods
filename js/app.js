b58c29560c85b8fa03c9d16a14943ba020e71f03
asfrazao <Nena@2020>
2026-03-17 09:26:49 -0300

    Ajuste de Html

---
+++ js/app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const yearElement = document.getElementById('currentYear');
+
+  if (yearElement) {
+    yearElement.textContent = new Date().getFullYear();
+  }
+});
